808e230e98d7afd43b4daa845bb21fa6aeef7fac
oluwatobi1 <akintunlesetobi@gmail.com>
2021-02-16 00:32:50 +0100

    5: router navigate button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'paymentForm', component:PaymentFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
